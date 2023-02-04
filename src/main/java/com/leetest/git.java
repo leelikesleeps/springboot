66e125fa60912857b843d6cdda9d0fabefd9d79e
lee <1587569978@qq.com>
2023-02-04 22:17:54 +0800

    two commit

--- src/main/java/com/leetest/git.java
+++ src/main/java/com/leetest/git.java
@@ -1,11 +1,13 @@
 package com.leetest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/boot")
 public class git {
+    @GetMapping
     public String z(){
         System.out.println("1");
         System.out.println("1");
